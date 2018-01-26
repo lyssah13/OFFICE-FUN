2d3f6bbc813d693d7c099440f18689b10814e0ec
malbardiaz <35084123+malbardiaz@users.noreply.github.com>
2018-01-25 19:15:52 -0500

    react santa js file

---
+++ src/components/SecretSanta.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+
+class SecretSanta extends Component{
+  render () {
+    return (
+
+      <form id="exchange">
+          <h1 >Secret Santa Generator</h1>
+            <div ClassName="name_email">
+              <label id="firstNameLabel" for="firstName" ClassName="label">Employee</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="Name"/>
+              <label id="firstNameLabel" for="firstName" ClassName="label">Email</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="name@example.com"/>
+            </div> <br/>
+            <div ClassName="name_email">
+              <label id="firstNameLabel" for="firstName" ClassName="label">Employee</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="Name"/>
+              <label id="firstNameLabel" for="firstName" ClassName="label">Email</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="name@example.com"/>
+            </div> <br/>
+            <div ClassName="name_email">
+              <label id="firstNameLabel" for="firstName" ClassName="label">Employee</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="Name"/>
+              <label id="firstNameLabel" for="firstName" ClassName="label">Email</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="name@example.com"/>
+            </div> <br/>
+            <div ClassName="name_email">
+              <label id="firstNameLabel" for="firstName" ClassName="label">Employee</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="Name"/>
+              <label id="firstNameLabel" for="firstName" ClassName="label">Email</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="name@example.com"/>
+            </div> <br/>
+            <div ClassName="name_email">
+              <label id="firstNameLabel" for="firstName" ClassName="label">Employee</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="Name"/>
+              <label id="firstNameLabel" for="firstName" ClassName="label">Email</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="name@example.com"/>
+            </div> <br/>
+            <div ClassName="name_email">
+              <label id="firstNameLabel" for="firstName" ClassName="label">Employee</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="Name"/>
+              <label id="firstNameLabel" for="firstName" ClassName="label">Email</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="name@example.com"/>
+            </div> <br/>
+            <div ClassName="name_email">
+              <label id="firstNameLabel" for="firstName" ClassName="label">Employee</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="Name"/>
+              <label id="firstNameLabel" for="firstName" ClassName="label">Email</label>
+              <input id="firstName" name="firstName" type="text" size="30" placeholder="name@example.com"/>
+            </div> <br/>
+
+            <div ClassName="button">
+              <button type="submit">Santa!</button>
+            </div>
+
+      </form>
+
+    )
+  }
+}
+
+export default SecretSanta;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import SecretSanta from './components/SecretSanta.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SecretSanta />, document.getElementById('root'));
 registerServiceWorker();
